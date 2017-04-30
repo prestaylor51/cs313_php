761300ab73c8f5de64040bec36608e2624e1acc5
prestaylor51 <prestaylor51@gmail.com>
2017-04-29 18:24:20 -0600

    semi-passable webpage

---
+++ web/preston-homepage.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="homepage.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<title>About Us</title>
+</head>
+<body>
+
+	<?php
+		require("home-nav.php");
+	?>
+
+	<div class="container-fluid">
+	<div class="jumbotron grey opac">
+		<h1>Preston Taylor</h1>
+		<p>"It's our wits that make us men."</p>
+		<p>-Malcom Wallace-</p>
+	</div>
+
+	</div>
+
+	<div class="container-fluid ">
+		<div>
+		<div class="row">
+			<div class="col-sm-4 center">
+				<div class="jumbotron opac smallJumbo">
+					<h2 class="hobbiesText ">Computers</h2>
+				</div>
+				
+				<img src="heatSink.jpg" class="img-rounded">
+			</div>
+
+			<div class="col-sm-4 center">
+				<div class="jumbotron opac smallJumbo">
+					<h2 class="hobbiesText" >Hunting</h2>
+				</div>
+				<img src="Hunting.jpg" class="img-rounded">
+			</div>
+
+			<div class="col-sm-4 center">
+				<div class="jumbotron opac smallJumbo">
+					<h2 class="hobbiesText">Family</h2>
+				</div>
+				<img src="">
+			</div>
+		</div>
+		</div>
+	</div>
+
+
+
+		
+</body>
+</html>
---
+++ web/project-index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="homepage.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<title>About Us</title>
+</head>
+<body>
+
+	<?php
+		require("home-nav.php");
+	?>
+
+	<div class="container-fluid">
+	<div class="jumbotron grey opac" >
+		<h1 class="grey">Projets Coming Soon</h1>
+	</div>
+
+	</div>
+
+
+		
+</body>
+</html>
