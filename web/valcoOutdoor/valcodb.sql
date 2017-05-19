2ff60dbadc31c5d672ad798f4faa791beeb836dc
prestaylor51 <prestaylor51@gmail.com>
2017-05-18 20:23:49 -0600

    changed admin to isadmin

--- web/valcoOutdoor/valcodb.sql
+++ web/valcoOutdoor/valcodb.sql
@@ -21,7 +21,7 @@ CREATE TABLE employee
 ,password		VARCHAR(20)		NOT NULL UNIQUE
 ,first_name 	VARCHAR(100)	NOT NULL
 ,last_name  	VARCHAR(100)	NOT NULL
-,admin			INT 			NOT NULL
+,isadmin			INT 			NOT NULL
 );
 
 
@@ -62,7 +62,7 @@ CREATE TABLE purchase
 
 -- INSERT EMPLOYEE--
 INSERT INTO employee
-(username, password, first_name, last_name, admin)
+(username, password, first_name, last_name, isadmin)
 VALUES
 ('pdawg', 'pdawgtheman', 'Preston','Taylor',1),
 ('bossman','12345', 'Val', 'Taylor', 1),
